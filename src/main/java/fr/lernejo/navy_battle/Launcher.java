fc8ed1f08156445f1023e59de1b0af2ceba6f4db
GaZotann <gazotan.minecraft@gmail.com>
2021-12-06 11:04:18 +0100

    git une troisieme upgrade du coverage

--- src/main/java/fr/lernejo/navy_battle/Launcher.java
+++ src/main/java/fr/lernejo/navy_battle/Launcher.java
@@ -1,13 +1,18 @@
 package fr.lernejo.navy_battle;
 
+import java.io.IOException;
+
 public class Launcher {
-    public static void main(String[] args){
+    public static void main(String[] args)throws IOException {
         if(args.length > 0){
             int port = Integer.parseInt(args[0]);
             try {
                ServeurHTTP serveurHTTP = new ServeurHTTP(port);
                serveurHTTP.create();
            }catch (Exception e){}
+        }
+        if (args.length <= 0){
+            throw new IllegalArgumentException("Il manque des arguments");
         }
     }
 }
--- src/test/java/fr/lernejo/navy_battle/LauncherTest.java
+++ src/test/java/fr/lernejo/navy_battle/LauncherTest.java
@@ -7,7 +7,15 @@ import org.junit.jupiter.api.Test;
 
 public class LauncherTest {
     @Test
+    void mainTest1(){
+        org.assertj.core.api.Assertions.assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> Launcher.main(new String[] {})).withMessage("Il manque des arguments");
+    }
+    @Test
     void mainTest2(){
+        org.assertj.core.api.Assertions.assertThatNoException().isThrownBy(() -> Launcher.main(new String[] {"9786"}));
+    }
+    @Test
+    void mainTest3(){
         String[] args = {"9876"};
         org.assertj.core.api.Assertions.assertThatNoException().isThrownBy(() -> new ServeurHTTP(9876).create());
     }
